b489136ddb031e3d4e3d91f27e2753678ab5cb0f
Sachin Vas <sachin@mobstac.com>
2019-06-14 10:46:36 +0530

    Fix name conflicts

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
 import {NativeModules} from 'react-native';
 
-console.log(NativeModules);
+const { NearBee } = NativeModules;
 
-const { RNNearBee } = NativeModules;
-
-export default RNNearBee;
+export default NearBee;
 
 // Create bundle manually for android
 // react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
